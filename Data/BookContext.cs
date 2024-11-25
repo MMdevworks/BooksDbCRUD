6ad1c3826b723a8cd251055e66a49de1663fd7ad
Marie <mmdevq@gmail.com>
2024-11-25 13:01:12 -0800

    data context

---
+++ Data/BookContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_1BooksDbCRUD.Data
+{
+    public class BookContext
+    {
+    }
+}
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +11,6 @@ namespace _11_1BooksDbCRUD.Models
     {
         public int CategoriesID { get; set; }
         public string? Name { get; set; }
+        public virtual ObservableCollectionListSource<Book> Books { get; set; }
     }
 }
